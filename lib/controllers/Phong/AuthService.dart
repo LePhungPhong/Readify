bc2eaf8b3577959418db64ea9af58eac78a2e720
dohoangthanh <dohoangthanh2306@gmail.com>
2025-05-26 19:31:11 +0700

    Update AboutPage UI

--- lib/controllers/Phong/AuthService.dart
+++ lib/controllers/Phong/AuthService.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:readify/models/Phong/user_model.dart';
 import 'dart:convert';
 import 'package:crypto/crypto.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   final CollectionReference _usersCollection = FirebaseFirestore.instance
@@ -30,7 +31,7 @@ class AuthService {
         // So sánh mật khẩu mã hóa
         final hashedInputPassword = hashPassword(password);
         if (storedPassword == hashedInputPassword) {
-          return UserModel.fromMap({
+          final user = UserModel.fromMap({
             'id': userData['id'] as int,
             'email': userData['email'] as String,
             'password': userData['password'] as String,
@@ -39,6 +40,11 @@ class AuthService {
             'created_at': userData['created_at'] as String?,
             'updated_at': userData['updated_at'] as String?,
           });
+          // Lưu user_id vào SharedPreferences
+          final prefs = await SharedPreferences.getInstance();
+          await prefs.setInt('user_id', user.id);
+
+          return user;
         }
       }
       return null; // Email hoặc mật khẩu không đúng
@@ -82,5 +88,29 @@ class AuthService {
       print('Đăng ký thất bại: $e');
       return 'Đăng ký thất bại: $e';
     }
+  }
+
+  // lấy thông tin người dùng hiện tại từ Firestore
+  Future<UserModel?> getCurrentUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    final userId = prefs.getInt('user_id');
+    if (userId == null) {
+      print('Không tìm thấy user_id trong SharedPreferences.');
+      return null;
+    }
+
+    try {
+      final doc = await _usersCollection.doc(userId.toString()).get();
+      if (doc.exists) {
+        final data = doc.data() as Map<String, dynamic>;
+        return UserModel.fromMap(data);
+      } else {
+        print('Không tìm thấy user trong Firestore với ID: $userId');
+      }
+    } catch (e) {
+      print('Lỗi khi lấy user từ Firestore: $e');
+    }
+
+    return null;
   }
 }
